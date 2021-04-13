3a94e36e72434bbd9e95320672c8a1296b68f60b
Gltched <78609482+Gltched@users.noreply.github.com>
2021-04-13 12:44:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,55 @@ import "./style.css";
 export default function App() {
   return (
     <div>
+      <div class="header">
+        <div class="content-centered">
+          <p class="header-content">
+            <a class="header-link" href="./index.html">
+              <img
+                class="content-centered-image"
+                src="/img/Gltched Drawing.png"
+                alt="Website logo"
+                width="40px"
+              />
+            </a>
+          </p>
+        </div>
+        <div class="content-centered">
+          <p class="header-content">
+            <a class="header-link" href="Skill-Points/index.html">
+              Real Life Skill Points
+            </a>
+          </p>
+        </div>
+        <div class="content-centered">
+          <p class="header-content">
+            <a class="header-link" href="Discord/index.html">
+              Discord
+            </a>
+          </p>
+        </div>
+        <div class="content-centered">
+          <p class="header-content">
+            <a class="header-link" href="Jam/index.html">
+              Game Jam
+            </a>
+          </p>
+        </div>
+        <div class="content-centered">
+          <p class="header-content">
+            <a class="header-link" href="Server/index.html">
+              Server
+            </a>
+          </p>
+        </div>
+        <div class="content-centered">
+          <p class="header-content">
+            <a class="header-link" href="SignUpForm/index.html">
+              Sign Up
+            </a>
+          </p>
+        </div>
+      </div>
       <h1 class="home-title">Dungeons & Dragons</h1>
     </div>
   );
